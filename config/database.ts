111395bce1195e4245fbd38012a8b0e107404f22
SlyB33 <skvmOmega@gmail.com>
2025-03-06 16:03:22 +0100

    ssl check

--- config/database.ts
+++ config/database.ts
@@ -36,6 +36,7 @@ export default ({ env }) => {
 				database: env("DATABASE_NAME", "strapi"),
 				user: env("DATABASE_USERNAME", "strapi"),
 				password: env("DATABASE_PASSWORD", "strapi"),
+				ssl: false,
 				// ssl: env.bool("DATABASE_SSL", false) && {
 				// 	key: env("DATABASE_SSL_KEY", undefined),
 				// 	cert: env("DATABASE_SSL_CERT", undefined),
